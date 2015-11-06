37dc09c9dd7518436a915f88cbd5e2524c4e3317
wanaaaa <wanaaaa@gmail.com>
2015-11-05 23:23:54 -0500

    asdf

--- vibez/app/controllers/posts_controller.rb
+++ vibez/app/controllers/posts_controller.rb
@@ -1,2 +1,45 @@
 class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+		@moods = ['happy', 'good', 'euphoric', 'ecstasy', 'so so']
+		@users = User.all.map do |x| 
+			[ x.username, x.id ]
+		end	
+	end
+
+	def create
+		@post = Post.new(post_params)
+	  @post.save
+	  redirect_to posts_path
+	end
+
+	def edit
+		@post = Article.find(params[:id])
+		@moods = ['happy', 'good', 'euphoric', 'ecstasy', 'so so']
+		@users = User.all.map do |x| 
+			[ x.username, x.id ]
+		end	
+	end
+
+	def update
+	  @post = Article.find(params[:id])
+	  @post.update(post_params)
+
+	  flash.notice = "Article '#{@post.title}' Updated!"
+
+
+	  redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post)
+			.permit(:song_title, :artist_name, :vibe, :like, :user_id)
+	end
 end
+
--- vibez/db/migrate/20151105204142_create_posts.rb
+++ vibez/db/migrate/20151105204142_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :song_title, null: false
-      t.string :artist_name, null: false
-      t.string :vibe, null: false
-      t.integer :like, null: false
+      t.string :song_title
+      t.string :artist_name
+      t.string :vibe
+      t.integer :like
 
       t.references :user, index:true, foreign_key: true
 
